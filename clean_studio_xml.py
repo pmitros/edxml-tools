4880150bc8a8e2575b40a23dcb423689716ab9fe
Piotr Mitros <piotr@mitros.org>
2017-09-21 14:35:32 -0400

    Clean Studio XML handles tar files

--- clean_studio_xml.py
+++ clean_studio_xml.py
@@ -1,15 +1,33 @@
 import argparse
 
+import tempfile
+import shutil
+import tarfile
+import os.path
+
 import helpers
 
 parser = argparse.ArgumentParser(description = "Clean up XML spat out by Studio.")
 parser.add_argument("base", help="Base directory of Studio-dumped XML")
 args = parser.parse_args()
 
+if args.base.endswith("tar.gz"):
+    TAR_FILE = True
+else:
+    TAR_FILE = False
+
+if TAR_FILE:
+    dirpath = tempfile.mkdtemp()
+    with tarfile.open(args.base) as tar:
+        tar.extractall(dirpath)
+    basepath = os.path.join(dirpath, "course")
+else:
+    basepath = args.base
+
 ## Helper functions ##
 
 # get root of course XML tree and load the XML for the entire course
-tree = helpers.load_xml_course(args.base)
+tree = helpers.load_xml_course(basepath)
 
 # Save the slugs used in the course, so we don't run into collisions while renaming
 helpers.save_url_name_slugs(tree)
@@ -24,7 +42,7 @@ helpers.propagate_display_between_parent_and_child(tree)
 helpers.propagate_display_to_url_name(tree)
 
 ## We'll clean up the filenames Studio assigned for our HTML files
-helpers.propagate_urlname_to_filename(tree, args.base)
+helpers.propagate_urlname_to_filename(tree, basepath)
 
 ## Add discussion tags where relevant. Add display names to discussions. 
 ## 
@@ -33,7 +51,7 @@ helpers.propagate_urlname_to_filename(tree, args.base)
 helpers.propagate_sibling_tags(tree)
 
 # We're done. Dump problems and course.xml back to the file system
-helpers.save_tree(args.base, tree)
+helpers.save_tree(basepath, tree)
 
 # And finally, dump the mapping file
 #
@@ -42,9 +60,14 @@ helpers.save_tree(args.base, tree)
 # if not os.path.exists(os.path.join(args.base, 'static')):
 #    os.mkdir(os.path.join(args.base, 'static'))
 
-helpers.save_url_name_map(args.base)
+helpers.save_url_name_map(basepath)
 
 # Now, we clean up a few JSON files. 
 for filename in ['policies/edx/policy.json', 'policies/edx/grading_policy.json']:
-    helpers.clean_json(args.base, filename)
-        
+    helpers.clean_json(basepath, filename)
+
+if TAR_FILE:
+    with tarfile.open(args.base, "w:gz") as tar:
+        tar.add(basepath, arcname='course')
+    
+    shutil.rmtree(dirpath)
